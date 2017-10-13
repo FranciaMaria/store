d81049eba0defa99c248217fb2260674812e10ff
FranciaMaria <mariafrancia88@gmail.com>
2017-10-13 19:00:02 +0200

    task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
+
+const appRoutes:Routes = [
+ { path: 'buyers', component: BuyersComponent },
+ { path: 'products', component: ProductsComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BuyersComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes
+    )], 
+  exports: [
+    LayoutComponent
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
